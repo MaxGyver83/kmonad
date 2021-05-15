ee9d0988dd00cda99c4d1af58ef4acfd96a51d6d
Max Schillinger <maxschillinger@web.de>
2021-05-15 12:47:51 +0200

    skip (, ), and _ in parser because they have special meanings

--- src/KMonad/Args/Parser.hs
+++ src/KMonad/Args/Parser.hs
@@ -192,11 +192,11 @@ shiftedNames = let f = second $ \kc -> KAround (KEmit KeyLeftShift) (KEmit kc) i
     cps = zip (map T.singleton ['A'..'Z'])
           [ KeyA, KeyB, KeyC, KeyD, KeyE, KeyF, KeyG, KeyH, KeyI, KeyJ, KeyK, KeyL, KeyM,
             KeyN, KeyO, KeyP, KeyQ, KeyR, KeyS, KeyT, KeyU, KeyV, KeyW, KeyX, KeyY, KeyZ ]
-    num = zip (map T.singleton "!\"§$%&/()=")
-          [ Key1, Key2, Key3, Key4, Key5, Key6, Key7, Key8, Key9, Key0 ]
-    oth = zip (map T.singleton ";:Ö°Ä'Ü+`_")
+    num = zip (map T.singleton "!\"§$%&/=")
+          [ Key1, Key2, Key3, Key4, Key5, Key6, Key7, Key0 ]
+    oth = zip (map T.singleton ";:Ö°Ä'Ü+`")
           [ KeyComma, KeyDot, KeySemicolon, KeyGrave, KeyApostrophe, KeyBackslash
-          , KeyLeftBrace, KeyRightBrace, KeyEqual, KeySlash]
+          , KeyLeftBrace, KeyRightBrace, KeyEqual]
 
 -- | Names for various buttons
 buttonNames :: [(Text, DefButton)]
