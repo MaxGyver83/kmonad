07541da95db8726e965358a0d8cd10b8956b6d0c
Max Schillinger <maxschillinger@web.de>
2021-05-15 22:35:56 +0200

    apply changes from Parser.hs also in Tokenizer.hs

--- src/KMonad/App/Parser/Tokenizer.hs
+++ src/KMonad/App/Parser/Tokenizer.hs
@@ -189,23 +189,24 @@ shiftedNames = let f = second $ \kc -> KAround (KEmit KeyLeftShift) (KEmit kc) i
     cps = zip (map T.singleton ['A'..'Z'])
           [ KeyA, KeyB, KeyC, KeyD, KeyE, KeyF, KeyG, KeyH, KeyI, KeyJ, KeyK, KeyL, KeyM,
             KeyN, KeyO, KeyP, KeyQ, KeyR, KeyS, KeyT, KeyU, KeyV, KeyW, KeyX, KeyY, KeyZ ]
-    num = zip (map T.singleton "!@#$%^&*")
-          [ Key1, Key2, Key3, Key4, Key5, Key6, Key7, Key8 ]
-    oth = zip (map T.singleton "<>:~\"|{}+?")
+    num = zip (map T.singleton "!\"§$%&/=")
+          [ Key1, Key2, Key3, Key4, Key5, Key6, Key7, Key0 ]
+    oth = zip (map T.singleton ";:Ö°Ä'Ü+`")
           [ KeyComma, KeyDot, KeySemicolon, KeyGrave, KeyApostrophe, KeyBackslash
-          , KeyLeftBrace, KeyRightBrace, KeyEqual, KeySlash]
+          , KeyLeftBrace, KeyRightBrace, KeyEqual]
 
 -- | Names for various buttons
 buttonNames :: [(Text, DefButton)]
 buttonNames = shiftedNames <> escp <> util
   where
     emitS c = KAround (KEmit KeyLeftShift) (KEmit c)
+    emitA c = KAround (KEmit KeyRightAlt) (KEmit c)
     -- Escaped versions for reserved characters
-    escp = [ ("\\(", emitS Key9), ("\\)", emitS Key0)
-           , ("\\_", emitS KeyMinus), ("\\\\", KEmit KeyBackslash)]
+    escp = [ ("\\(", emitS Key8), ("\\)", emitS Key9)
+           , ("\\_", emitS KeySlash), ("\\\\", emitA KeyMinus)]  -- "\" is AltGr+KeyMinus
     -- Extra names for useful buttons
     util = [ ("_", KTrans), ("XX", KBlock)
-           , ("lprn", emitS Key9), ("rprn", emitS Key0)]
+           , ("lprn", emitS Key8), ("rprn", emitS Key9)]
 
 
 
